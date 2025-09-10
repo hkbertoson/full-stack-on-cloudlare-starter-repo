4762bf3b6ec108376160daac4becf5e0e540039d
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2025-09-10 17:18:59 -0500

    Refactor DestinationEvaluationWorkflow to improve code structure and readability

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,16 +1,38 @@
-import { collectDestinationInfo } from "@/helpers/browser-render";
 import {
 	WorkflowEntrypoint,
 	type WorkflowEvent,
 	type WorkflowStep,
 } from "cloudflare:workers";
+import { aiDestinationChecker } from "@/helpers/ai-destination-checker";
+import { collectDestinationInfo } from "@/helpers/browser-render";
 
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
-	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<
+	Env,
+	DestinationStatusEvaluationParams
+> {
+	async run(
+		event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>,
+		step: WorkflowStep,
+	) {
+		const collectedData = await step.do(
+			"Collect rendered destination page data",
+			async () => {
+				return collectDestinationInfo(this.env, event.payload.destinationUrl);
+			},
+		);
 
-		const collectedData = await step.do("Collect rendered destination page data", async () => {
-			return collectDestinationInfo(this.env, event.payload.destinationUrl)
-		});
+		const aiStatus = await step.do(
+			"Use AI to check status of page",
+			{
+				retries: {
+					limit: 0,
+					delay: 0,
+				},
+			},
+			async () => {
+				return await aiDestinationChecker(this.env, collectedData.bodyText);
+			},
+		);
 		console.log(collectedData);
 	}
 }
