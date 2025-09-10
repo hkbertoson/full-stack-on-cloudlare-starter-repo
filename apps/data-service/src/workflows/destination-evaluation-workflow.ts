cb47a9f5364b1b3a842a80c0fc846061791a274c
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2025-09-10 16:46:22 -0500

    Add DestinationEvaluationWorkflow class to handle destination evaluation logic

---
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -0,0 +1,19 @@
+import {
+	WorkflowEntrypoint,
+	type WorkflowEvent,
+	type WorkflowStep,
+} from "cloudflare:workers";
+
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
+	async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+
+		const collectedData = await step.do("Collect rendered destination page data", async () => {
+			console.log("Collecting rendered destination page data");
+			return {
+				"dummydata": "dummydata",
+
+			}
+		});
+		console.log(collectedData);
+	}
+}
