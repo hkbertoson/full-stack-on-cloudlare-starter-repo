d1ea3a47fc35ce426ef87544e378d4ef116d918c
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2025-09-10 16:46:20 -0500

    Refactor import statement for handleLinkClick and improve code formatting

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,8 +1,10 @@
 import { WorkerEntrypoint } from "cloudflare:workers";
 import { initDatabase } from "@repo/data-ops/database";
 import { QueueMessageSchema } from "@repo/data-ops/zod-schema/queue";
+import { handleLinkClick } from "@/queue-handlers/link-clicks";
 import { App } from "./hono/app";
-import { handleLinkClick } from "./queue-handlers/link-clicks";
+
+export { DestinationEvaluationWorkflow } from "@/workflows/destination-evaluation-workflow";
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	constructor(ctx: ExecutionContext, env: Env) {
@@ -17,11 +19,11 @@ export default class DataService extends WorkerEntrypoint<Env> {
 			const parsedEvent = QueueMessageSchema.safeParse(message.body);
 			if (parsedEvent.success) {
 				const event = parsedEvent.data;
-				if (event.type === 'LINK_CLICK') {
-					await handleLinkClick(this.env, event)
+				if (event.type === "LINK_CLICK") {
+					await handleLinkClick(this.env, event);
 				}
 			} else {
-				console.error(parsedEvent.error)
+				console.error(parsedEvent.error);
 			}
 		}
 	}
