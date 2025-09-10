271f11198f263e02bd481ca113b867c0199be44f
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2025-09-10 17:55:18 -0500

    Refactor code structure for improved readability and maintainability

---
+++ packages/data-ops/src/queries/evalutations.ts
@@ -0,0 +1,58 @@
+import { and, desc, eq, gt } from "drizzle-orm";
+import { nanoid } from "nanoid";
+import { getDb } from "@/db/database";
+import { destinationEvaluations } from "@/drizzle-out/schema";
+
+
+export async function addEvaluation(data: {
+    linkId: string,
+    accountId: string,
+    destinationUrl: string,
+    status: string,
+    reason: string
+}) {
+    const db = getDb()
+    const id = nanoid();
+
+    await db.insert(destinationEvaluations).values({
+        id: id,
+        linkId: data.linkId,
+        accountId: data.accountId,
+        destinationUrl: data.destinationUrl,
+        status: data.status,
+        reason: data.reason,
+    })
+    return id;
+}
+
+export async function getNotAvailableEvaluations(accountId: string) {
+    const db = getDb()
+    const result = await db
+        .select()
+        .from(destinationEvaluations)
+        .where(
+            and(
+                eq(destinationEvaluations.accountId, accountId),
+                eq(destinationEvaluations.status, "NOT_AVAILABLE_PRODUCT")
+            )
+        )
+        .orderBy(desc(destinationEvaluations.createdAt))
+        .limit(20)
+    return result;
+}
+
+export async function getEvaluations(accountId: string, createdBefore?: string) {
+    const db = getDb()
+    const conditions = [eq(destinationEvaluations.accountId, accountId)]
+    if (createdBefore) {
+        conditions.push(gt(destinationEvaluations.createdAt, createdBefore))
+    }
+
+    const result = await db
+        .select()
+        .from(destinationEvaluations)
+        .where(and(...conditions))
+        .orderBy(desc(destinationEvaluations.createdAt))
+        .limit(25)
+    return result;
+}
